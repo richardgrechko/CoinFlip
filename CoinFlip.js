c66a9e7dc9c3fbad4b318cad8df5cd7c66a4a632
RichardGrechko736 <162537734+richardgrechko@users.noreply.github.com>
2025-04-10 16:33:05 +0500

    Update CoinFlip.js

--- CoinFlip.js
+++ CoinFlip.js
@@ -1,20 +1,20 @@
 function CoinFlip(x)
 {
-	let heads = 0, tails = 0, random;
+	let heads = 1, tails = 1, random;
 	for (let i = 0; i < x; i++)
 	{
 		random = Math.random();
 		if (random <= 0.5)
 		{
-			heads = 0;
+			console.log("Heads gotten:", heads, `[1 in ${2**heads}]`);
+			heads = 1;
 			tails++;
-			console.log("T", tails, `[1 in ${2**tails}]`);
 		}
 		else
 		{
-			tails = 0;
+			console.log("Tails gotten:", tails, `[1 in ${2**tails}]`);
+			tails = 1;
 			heads++;
-			console.log("H", heads, `[1 in ${2**heads}]`);
 		}
 	}
 }
