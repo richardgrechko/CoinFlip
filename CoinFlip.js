ab1dc77a9d715595a74bb27c5e4e3dc07a2bb740
RichardGrechko736 <162537734+richardgrechko@users.noreply.github.com>
2025-09-02 12:01:01 +0500

    Update CoinFlip.js

--- CoinFlip.js
+++ CoinFlip.js
@@ -1,20 +1,28 @@
 function CoinFlip(x)
 {
-	let heads = 1, tails = 1, random;
+	let heads = 1, tails = 1, highestHeads = 0, highestTails = 0, random;
 	for (let i = 0; i < x; i++)
 	{
 		random = Math.random();
 		if (random <= 0.5)
 		{
-			heads = 1;
-			tails++;
-			console.log("T", tails, `[1 in ${2**tails}]`);
-		}
-		else
-		{
 			tails = 1;
 			heads++;
 			console.log("H", heads, `[1 in ${2**heads}]`);
+			if (highestHeads < heads) {
+				highestHeads = heads
+			}
+		}
+		else
+		{
+			heads = 1;
+			tails++;
+			console.log("T", tails, `[1 in ${2**tails}]`);
+			if (highestTails < tails) {
+				highestTails = tails
+			}
 		}
 	}
+	console.log("Highest heads", highestHeads, `[1 in ${2**highestHeads}]`);
+	console.log("Highest tails", highestTails, `[1 in ${2**highestTails}]`);
 }
