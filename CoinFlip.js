0fe8ea2842f4f1a00285efa00068d5e56beda49a
RichardGrechko736 <162537734+richardgrechko@users.noreply.github.com>
2025-09-02 12:03:56 +0500

    Update CoinFlip.js

--- CoinFlip.js
+++ CoinFlip.js
@@ -1,28 +1,58 @@
-function CoinFlip(x)
-{
-	let heads = 1, tails = 1, highestHeads = 0, highestTails = 0, random;
-	for (let i = 0; i < x; i++)
+class CoinFlip {
+	static new(x)
 	{
-		random = Math.random();
-		if (random <= 0.5)
+		let heads = 1, tails = 1, highestHeads = 0, highestTails = 0, random;
+		for (let i = 0; i < x; i++)
 		{
-			tails = 1;
-			heads++;
-			console.log("H", heads, `[1 in ${2**heads}]`);
-			if (highestHeads < heads) {
-				highestHeads = heads
+			random = Math.random();
+			if (random <= 0.5)
+			{
+				tails = 0;
+				heads++;
+				console.log("H", heads, `[1 in ${2**heads}]`);
+				if (highestHeads < heads) {
+					highestHeads = heads
+				}
+			}
+			else
+			{
+				heads = 0;
+				tails++;
+				console.log("T", tails, `[1 in ${2**tails}]`);
+				if (highestTails < tails) {
+					highestTails = tails
+				}
 			}
 		}
-		else
+		console.log("Highest heads", highestHeads, `[1 in ${2**highestHeads}]`);
+		console.log("Highest tails", highestTails, `[1 in ${2**highestTails}]`);
+	};
+	static Letters(x)
+	{
+		let heads = 1, tails = 1, highestHeads = 0, highestTails = 0, result = "", random;
+		for (let i = 0; i < x; i++)
 		{
-			heads = 1;
-			tails++;
-			console.log("T", tails, `[1 in ${2**tails}]`);
-			if (highestTails < tails) {
-				highestTails = tails
+			random = Math.random();
+			if (random <= 0.5)
+			{
+				tails = 0;
+				heads++;
+				result += "H";
+				if (highestHeads < heads) {
+					highestHeads = heads
+				}
+			}
+			else
+			{
+				heads = 0;
+				tails++;
+				result += "T";
+				if (highestTails < tails) {
+					highestTails = tails
+				}
 			}
 		}
-	}
-	console.log("Highest heads", highestHeads, `[1 in ${2**highestHeads}]`);
-	console.log("Highest tails", highestTails, `[1 in ${2**highestTails}]`);
+		console.log("Highest heads", highestHeads, `[1 in ${2**highestHeads}]`);
+		console.log("Highest tails", highestTails, `[1 in ${2**highestTails}]`);
+	};
 }
